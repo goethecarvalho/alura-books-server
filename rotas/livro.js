d6c78414cac02dd5439857adcc1ed85bae6e42e4
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-22 15:48:02 -0300

    Node.js - Controladores

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,15 +1,9 @@
 const { Router } = require("express")
+const { getLivros } = require("../controladores/livro")
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    try {
-        res.send('Olá mundo da Alura do Brasil!')
-    } catch (error) {
-        res.status(500)
-        res.send(error.message)
-    }
-})
+router.get('/', getLivros)
 
 router.post('/', (req, res) => {
     res.send('Você fez uma requisição do tipo POST')
