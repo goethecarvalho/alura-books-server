dbc0abb2dfe887cb2857c504a1cfe2a031e46296
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-22 15:42:26 -0300

    Node.js - Rotas de post, patch e delete

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,11 +1,26 @@
-// livro.js
-
 const { Router } = require("express")
 
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send('Olá mundo da Alura do Brasil!')
+    try {
+        res.send('Olá mundo da Alura do Brasil!')
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+})
+
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+router.patch('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo PATCH')
+})
+
+router.delete('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo DELETE')
 })
 
 module.exports = router
