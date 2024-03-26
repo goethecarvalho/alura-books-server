adb7772e99efbbb3bce6d3f22e99cf6a2c28beb6
Goethe Carvalho <goethecarvalho@gmail.com>
2024-03-26 15:38:06 -0300

    Node.js - Lidando com erros

--- controladores/livro.js
+++ controladores/livro.js
@@ -9,7 +9,7 @@ function getLivros(req, res) {
     } catch (error) {
         res.status(500)
         res.send(error.message)
-    } 
+    }
 }
 
 function getLivro(req, res) {
@@ -32,9 +32,15 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("Livro inserido com sucesso")
+        if(req.body.nome){
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("Campo nome é obrigatório")
+        }
+        
     } catch (error) {
         res.status(500)
         res.send(error.message)
